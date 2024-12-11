763dce92b81d646ffe0dd0e48b6722e94125bc00
Nilesh Deshmukh <65888258+nileshdeshmukh-code@users.noreply.github.com>
2024-12-11 11:45:39 +0530

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,52 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage;
+
+
+namespace AzureStorage.API
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var key = Environment.GetEnvironmentVariable("Key"); 
+            var accountName = Environment.GetEnvironmentVariable("AccountName");
+
+            var blobServiceClient = new BlobServiceClient(new Uri($"https://{accountName}.blob.core.windows.net"), new StorageSharedKeyCredential(accountName, key));
+
+            services.AddSingleton<BlobServiceClient>(blobServiceClient);
+
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
